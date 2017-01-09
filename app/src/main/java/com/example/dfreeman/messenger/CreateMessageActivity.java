542701b97b6a3af7b71a50f10d0b0f51dc2d3e0f
dfreemanRIIS <dfreeman@riis.com>
2017-01-09 15:33:56 -0500

    Create basic layout

--- app/src/main/java/com/example/dfreeman/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/example/dfreeman/messenger/CreateMessageActivity.java
@@ -2,6 +2,7 @@ package com.example.dfreeman.messenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class CreateMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+        //
     }
 }
